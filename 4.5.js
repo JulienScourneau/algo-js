1ac67f4ef93f520ee1f103d4fb29cf0c93fa7dd2
Julien Scourneau <scourneaujulien@outlook.com>
2022-07-15 15:44:12 +0200

    debug 4.5

--- 4.5.js
+++ 4.5.js
@@ -1,7 +1,8 @@
 function calcDistance(x1, x2, y1, y2) {
-    console.log("End function");
-    return Math.hypot(x2-x1, y2 - y1)
+    let x = x1 - x2
+    let y = y1 - y2
+    return Math.sqrt(x * x + y * y)
 
 }
 
-console.log(calcDistance(1, 1, 3, 1));
+console.log(calcDistance(1, 2, 1, 2));
