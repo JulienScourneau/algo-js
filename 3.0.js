d60434d44bb7cb772be9ac28e5aac5518b23f4f5
Julien Scourneau <scourneaujulien@outlook.com>
2022-07-15 10:08:48 +0200

    Finished 3.0

---
+++ 3.0.js
@@ -0,0 +1,5 @@
+let arr = ["Anthony Denis","Axel Kirac","Daniela Co","Doriano Vanvolcksom", "Dylan Marcot", "Eddy Vervoort", "Hazem Maddouri","Julien Scourneau", "Julien Elinckx", "Kamilla Moraes", "Luca Friquet", "Quentin Macq", "Rinaldo Benacceta", "Vincent Devilers"]
+
+for(let elem of arr){
+    console.log("Welcome to BeCode " + elem)
+}
